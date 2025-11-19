c6a76e220284232d1578a89c4c195095dc8d8232
Dayana Garcia <dayanagr9705@gmail.com>
2025-11-19 18:36:55 -0500

    add modify for library SendGrid

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -2,6 +2,8 @@
 using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using SendGrid.Helpers.Mail;
+using SendGrid;
 using TodoApp.API.Interfaces;
 
 namespace TodoApp.API.Services
@@ -22,19 +24,27 @@ namespace TodoApp.API.Services
             var user = _config["Smtp:User"];
             var pass = _config["Smtp:Password"];
             var from = _config["Smtp:From"];
-            Console.WriteLine(pass);
-            using var client = new SmtpClient(host, port)
-            {
-                EnableSsl = true,
-                Credentials = new NetworkCredential(user, pass)
-            };
+            //Console.WriteLine(pass);
+            //using var client = new SmtpClient(host, port)
+            //{
+            //    EnableSsl = true,
+            //    Credentials = new NetworkCredential(user, pass)
+            //};
 
-            var mail = new MailMessage(from, to, subject, body)
-            {
-                IsBodyHtml = false
-            };
+            //var mail = new MailMessage(from, to, subject, body)
+            //{
+            //    IsBodyHtml = false
+            //};
 
-            await client.SendMailAsync(mail);
+            //await client.SendMailAsync(mail);
+            var client = new SendGridClient(pass);
+            var fromEmail = new EmailAddress(from);
+            var subjectEmail = subject;
+            var toEmail = new EmailAddress(to);
+            var plainTextContent = body;
+            //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(fromEmail, toEmail, subjectEmail, plainTextContent, null);
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
